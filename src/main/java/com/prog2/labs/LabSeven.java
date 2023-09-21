889944933d3682e7d36ce10fafe204d9540cf3ce
HSterben <142906656+HSterben@users.noreply.github.com>
2023-09-21 13:26:25 -0400

    Update LabSeven.java

--- src/main/java/com/prog2/labs/LabSeven.java
+++ src/main/java/com/prog2/labs/LabSeven.java
@@ -1,5 +1,7 @@
 package com.prog2.labs;
 
+import java.util.ArrayList;
+
 /**
  * @author adinashby
  *
@@ -19,7 +21,32 @@ public class LabSeven {
 	 * Please refer to the README file for question(s) description
 	 */
 
-	public static int kthSmallestSubarraySum(int[] nums, int k) {
-		return 0;
+	public static int kthSmallestSubarraySum(int[] nums, int k) {  
+            ArrayList<Integer> array = new ArrayList<>();
+            
+            int length = nums.length;
+            for (int i = 0; i < length; i++) {
+                for (int j = 0; true; j++) {
+                    int sum = 0;
+                    for (int l = 0; l <= i; l++) {
+                        sum += nums[j + l];
+                    }
+                    array.add(sum);
+                    if (((j + 1) + i) >= nums.length) {
+                        break;
+                    }
+                }
+            }
+            
+            for (int i = 0; i < array.size(); i++) {
+                for (int j = 0; j < array.size() - 1; j++) {
+                    if (array.get(i) < array.get(j)) {
+                        int temp = array.get(i);
+                        array.set(i, array.get(j));
+                        array.set(j, temp);
+                    }
+                }
+            }
+            return array.get(k - 1);
 	}
 }
